c7ae0d6bdd423a0908792cf6d1d4025ff5a5906c
PedroDNRusso <pedrodnrusso@gmail.com>
2025-05-15 23:52:20 -0300

    cadastro teste

--- Test/script.js
+++ Test/script.js
@@ -1,3 +1,4 @@
+const uri = 'http://localhost:3000';
 const togglePassword = document.querySelector("#togglePassword");
 const password = document.querySelector("#senha");
 
@@ -7,3 +8,28 @@ togglePassword.addEventListener("click", function (e) {
   password.setAttribute("type", type);
   this.classList.toggle("fa-eye-slash");
 });
+
+function cadastrar(){
+const form = document.querySelector('#cadastro form')
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    const dados = {
+        nome: form.nome.value,
+        email: form.email.value,
+        senha: form.senha.value,
+    }
+    fetch('http://localhost:3000/cadastro', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(dados)
+    })
+        .then(resp => resp.status)
+        .then(resp => {
+            if (resp == 201)
+                alert('Cadastro feito com sucesso!');
+            else
+                alert('Erro ao cadastrar!');
+                window.location.reload();
+        })
+})
+}
