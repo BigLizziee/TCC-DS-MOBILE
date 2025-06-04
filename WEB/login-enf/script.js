9e453a6596fe9ad834edeaf2c978eaf1463bdafd
Aluno <pedrodnrusso@gmail.com>
2025-06-04 15:36:14 -0300

    atts login-enf

---
+++ WEB/login-enf/script.js
@@ -0,0 +1,56 @@
+const uri = 'http://localhost:3000'; 
+
+const togglePassword = document.querySelector("#togglePassword");
+const password = document.querySelector("#senha");
+
+function login() {
+    const form = document.querySelector('#formLogin');
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+
+        const dados = {
+            email: form.email.value,
+            senha: form.senha.value,
+        };
+
+        try {
+            const response = await fetch(`${uri}/login`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(dados),
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                alert('Login bem-sucedido!');
+
+                // Salva nome e email corretos vindos da API
+                sessionStorage.setItem("usuario", JSON.stringify({
+                    id: data.id, // Certifique-se de que o ID está sendo retornado pela API
+                    nome: data.nome,
+                    email: data.email,
+                    senha: data.senha // Armazena a senha para futuras requisições
+                }));
+
+                window.location.href = '../ui/index.html'; // Redireciona para o perfil
+            } else {
+                alert(data.message || 'Email ou senha inválidos.');
+            }
+        } catch (err) {
+            console.error('Erro ao fazer login:', err);
+            alert('Erro ao fazer login.');
+        }
+    });
+}
+
+
+
+togglePassword.addEventListener("click", function (e) {
+  const type =
+    password.getAttribute("type") === "password" ? "text" : "password";
+  password.setAttribute("type", type);
+  this.classList.toggle("fa-eye-slash");
+});
+
+document.addEventListener('DOMContentLoaded', login);
