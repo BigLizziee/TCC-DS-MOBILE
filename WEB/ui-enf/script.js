24b7ff1d6f3aeee0580cf5aebb671a6bea6eaf8a
Aluno <pedrodnrusso@gmail.com>
2025-06-10 09:29:15 -0300

    atts ui-enf

--- WEB/ui-enf/script.js
+++ WEB/ui-enf/script.js
@@ -3,7 +3,7 @@ const enfermeiro = JSON.parse(sessionStorage.getItem("enfermeiro"));
 if (!enfermeiro) {
   window.location.href = "../login-enf/index.html";
 } else {
-  document.getElementById("ecip").textContent = enfermeiro.ecip;
+  document.getElementById("ecip").textContent = enfermeiro.id;
   document.getElementById("nome").textContent = enfermeiro.nome;
   document.getElementById("area").textContent = enfermeiro.area;
   document.getElementById("email").textContent = enfermeiro.email;
