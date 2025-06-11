a2fbce627cf9ea20db1c15e2dffa6a288436f282
Aluno <pedrodnrusso@gmail.com>
2025-06-11 11:30:44 -0300

    atts

--- WEB/ui-enf/script.js
+++ WEB/ui-enf/script.js
@@ -4,6 +4,8 @@ if (!enfermeiro) {
   window.location.href = "../login-enf/index.html";
 } else {
   document.getElementById("nome").textContent = enfermeiro.nome;
+  document.getElementById("ecip").textContent = enfermeiro.ecip;
+  document.getElementById("id").textContent = enfermeiro.id;
 }
 
 function logout() {
